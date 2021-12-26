ce6b8fdacac377597a8219c2b6025c44ddca16d3
Satyam <satyam985@gmail.com>
2021-12-26 16:24:26 +0530

    Reverse the changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,24 @@
 <template>
   <div>
-    <!-- <Header @sidenavToggle="displaySidenav = !displaySidenav" />
-    <Sidenav :show="displaySidenav" @close="displaySidenav = false" /> -->
+    <Header @sidenavToggle="displaySidenav = !displaySidenav" />
+    <Sidenav :show="displaySidenav" @close="displaySidenav = false" />
     <Nuxt />
   </div>
 </template>
 <script>
-// import Header from "@/components/navigation/Header";
-// import Sidenav from "@/components/navigation/Sidenav";
-// export default {
-//   components: {
-//     Header,
-//     Sidenav,
-//   },
-//   data() {
-//     return {
-//       displaySidenav: false,
-//     };
-//   },
-// };
-//
+import Header from "@/components/navigation/Header";
+import Sidenav from "@/components/navigation/Sidenav";
+export default {
+  components: {
+    Header,
+    Sidenav,
+  },
+  data() {
+    return {
+      displaySidenav: false,
+    };
+  },
+};
 </script>
 
 <style>
