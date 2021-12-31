ab966aa86cf3196311b308549ca7e19f17ae18b5
Satyam <satyam985@gmail.com>
2021-12-31 17:00:49 +0530

    fixing the logout timer

--- store/index.js
+++ store/index.js
@@ -97,22 +97,17 @@ const createStore = () => {
             localStorage.setItem("token", result.idToken);
             localStorage.setItem(
               "tokenExpiration",
-              new Date().getTime() + result.expiresIn * 1000
+              new Date().getTime() + Number.parseInt(result.expiresIn) * 1000
             );
             Cookie.set("jwt", result.idToken);
             Cookie.set(
               "expirationDate",
-              new Date().getTime() + result.expiresIn * 1000
+              new Date().getTime() + Number.parseInt(result.expiresIn) * 1000
             );
-            vuexContext.dispatch("setLogoutTimer", result.expiresIn * 1000);
           })
           .catch((e) => console.log(e));
       },
-      setLogoutTimer(vuexContext, duration) {
-        setTimeout(() => {
-          vuexContext.commit("clearToken");
-        }, duration);
-      },
+
       initAuth(vuexContext, req) {
         let token;
         let expirationDate;
@@ -134,15 +129,11 @@ const createStore = () => {
         } else {
           token = localStorage.getItem("token");
           expirationDate = localStorage.getItem("tokenExpiration");
-          if (new Date().getTime() > +expirationDate || !token) {
-            return;
-          }
         }
-
-        vuexContext.dispatch(
-          "setLogoutTimer",
-          +expirationDate - new Date().getTime()
-        );
+        if (new Date().getTime() > +expirationDate || !token) {
+          vuexContext.commit("clearToken");
+          return;
+        }
         vuexContext.commit("setToken", token);
       },
     },
